707c62595ab1b8acac950d041eaf17fe79a9e20d
Ashutosh Soni <ashusoni1892@gmail.com>
2026-08-28 23:03:02 +0530

    procrastinating a lot, but lets now lock in with properties of inheritance

--- mike_shah_cpp_course/classes/inheritance.cpp
+++ mike_shah_cpp_course/classes/inheritance.cpp
@@ -5,12 +5,12 @@ class cat{
         }
     void meow(){
         std::cout<<"miau"<<std::endl;
-    }
+    } 
     void walk(){
         x+=1;
         y+=1;
     }
-    float x,y;
+    float x,y;  // 4+4 bytes
 };
 
         /*
@@ -28,7 +28,7 @@ class Persian: public cat{
     public:
         void purr(){
             std::cout<<"Purrr...."<<std::endl;
-        }
+        }// added no extra variables in this class, so 4+4 bytes as inherited from cat
 };
 class Berry : public Persian { 
     // persian ka bilota 
@@ -40,16 +40,23 @@ public:
         std::cout<<"nom nom nom nom"<<std::endl;
         // his greed sickens me -_-
     }
+    // isme bhi no extra variables so the usual 4+4 bytes from cat and 0 bytes from Persian  
 };
 int main(){
     Persian cat1;
     cat1.meow();
     cat1.walk();
     cat1.purr();
+
     Berry cat1_ka_bilota;
     cat1_ka_bilota.eat();
     cat1_ka_bilota.sleep();
     cat1_ka_bilota.meow();
     cat1_ka_bilota.purr();
+
+    std::cout<<sizeof(cat)<<std::endl;
+    std::cout<<sizeof(Persian)<<std::endl;
+    std::cout<<sizeof(Berry)<<std::endl;
+
     return 0;
 }
